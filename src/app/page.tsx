01b30f07c31b982f9d03d8a98839e4832cddd664
unknown <jackb.doughty@btinternet.com>
2025-09-29 23:02:30 +0100

    biome fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    navigationMenuTriggerStyle
-} from "@/components/ui/navigation-menu"
-import Image from 'next/image'
-import Link from "next/link"
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 export default function Home() {
-    return (
-        <div className="max-w-4xl mx-auto px-4">
-            <div className="flex items-center justify-between">
-                <Image src="/jlogo.svg" alt="JD" width={60} height={60} />
-                <NavigationMenu>
-                    <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                                <Link href="/">Projects</Link>
-                            </NavigationMenuLink>
-                            <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                                <Link href="/">Posts</Link>
-                            </NavigationMenuLink>
-                            <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                                <Link href="/">Contact</Link>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
-            </div>
-        </div>
-    );
+  return (
+    <div className="max-w-4xl mx-auto px-4">
+      <div className="flex items-center justify-between">
+        <Image src="/jlogo.svg" alt="JD" width={60} height={60} />
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/">Projects</Link>
+              </NavigationMenuLink>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/">Posts</Link>
+              </NavigationMenuLink>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/">Contact</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
 }
